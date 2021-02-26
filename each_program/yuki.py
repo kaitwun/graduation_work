6f703241782b265c0fab1c18d612c640b9331ba2
Yuki hayashi <smtwtfs1414@gmail.com>
2021-02-26 22:31:08 +0900

    yuki.py追加

---
+++ each_program/yuki.py
@@ -0,0 +1,25 @@
+# flaskをimportしてflaskを使えるようにする
+from flask import Flask, render_template 
+import sqlite3
+
+# app Flaskを定義して使えるようにします。
+# flaskクラスのインスタンスを使ってappという変数に代入している
+app = Flask(__name__)
+
+@app.route('/')
+def new_post():
+    return "New"
+
+@app.route("/top")
+def top():
+    return "New POST（投稿）"
+
+
+
+@app.route("/introduce")
+def tpl():
+    return render_template("introduce.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
