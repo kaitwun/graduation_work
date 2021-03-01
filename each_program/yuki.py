0f7abe29f9a02fa7ed6a0157d8c8ac1b6989027e
Yuki hayashi <smtwtfs1414@gmail.com>
2021-03-01 16:29:04 +0900

    each_proguramへコピーして実行

--- each_program/yuki.py
+++ each_program/yuki.py
@@ -7,21 +7,21 @@ import sqlite3
 app = Flask(__name__)
 
 
-@app.route("/top")
+@app.route("/")
 def top():
-    conn = sqlite3.connect("graduation_work.db") #DBに接続
+    conn = sqlite3.connect("graduation_work copy.db") #DBに接続
     c = conn.cursor() #DBの中身をみれるようにする
     c.execute("select image, title from posts")
     #取得した値を変数に代入
     post = []
     for item in c.fetchall(): #すべてのデータを辞書型に整形
-        post.append({"image":post[0],"title":post[1]})
+        post.append({"image":item[0],"title":item[1]})
 
     # image = c.fetchall()#全部取って参る
     c.close() #DB接続終了
     print(post) #ターミナル上で確認
     #DBからimageのデータを取得
-        return render_template("top.html",tpl_post = post)
+    return render_template("top.html",tpl_post = post)
 
     
 
