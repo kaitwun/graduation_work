22b310160d1ef90ab688c28f327868ed9a4e6069
Mika Fukuhara <tougenkyou.mika@gmail.com>
2021-03-03 13:54:57 +0900

    mika.py6.7変更

--- each_program/mika.py
+++ each_program/mika.py
@@ -3,9 +3,9 @@ import sqlite3
 # flaskをimportしてflaskを使えるようにする
 from flask import Flask , render_template , request , redirect , session, url_for, Blueprint
 
-# Blueprintオブジェクトを生成
-#app = Blueprint('mika' , __name__)
+#Blueprintオブジェクトを生成
+app = Blueprint('mika' , __name__)
 
 #@app.route('mika')
-#def new_post():
-    #return render_template "base.html"
+#def new_post():    
+    #return render_template "base.html
