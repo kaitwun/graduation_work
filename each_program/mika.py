350deaa63a8715333ff25d6f97de133d1177d4d1
Kaito Misato <kaito20000721@gmail.com>
2021-03-04 23:30:24 +0900

    edit mika.py

--- each_program/mika.py
+++ each_program/mika.py
@@ -6,3 +6,7 @@ from flask import Flask , render_template , request , redirect , session, url_fo
 #Blueprintオブジェクトを生成
 app = Blueprint('mika' , __name__)
 
+@app.route("/level1", ["GET"])
+def level1():
+    conn = sqlite3.connect("graduation_work.db")
+    c = conn.cursor()
