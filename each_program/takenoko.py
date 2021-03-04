08dc2d007572e7bcb249367340b029b0fbe96c00
Takenoko Satono <isono.namiko@gmail.com>
2021-03-04 16:05:33 +0900

    投稿詳細ページの編集

--- each_program/takenoko.py
+++ each_program/takenoko.py
@@ -1,6 +1,9 @@
 # flaskをimportしてflaskを使えるようにする
 from flask import Flask, request, render_template, redirect, session, url_for, Blueprint 
 import sqlite3
+import datetime
+now = datetime.datetime.now()
+now = now.strftime("%Y-%m-%d %H:%M:%S")
 
 # app Flaskを定義して使えるようにします。
 # flaskクラスのインスタンスを使ってappという変数に代入している
@@ -26,7 +29,7 @@ def new_post():
         location =  request.form.get("location")
         category =  request.form.get("category")
         date =  request.form.get("date")
-        conn = sqlite3.connect("gradueathon_works.db")
+        conn = sqlite3.connect("graduation_work.db")
         c = conn.cursor()
         c.execute("INSERT INTO posts values(null,?,?,?,?,?,?,?,?,?,)",(user_id, title, food, image, main_evaluation, main_comment, location, category, date))
         conn.commit()
@@ -34,21 +37,38 @@ def new_post():
         return render_template("intoduce.html")
 
 
-@app.route("/introduce/<int:post_id>", methods =["GET"])
+@app.route("/introduce/<int:post_id>", methods =["GET","POST"])
 def intoduce(post_id):
         conn = sqlite3.connect("graduation_work.db")
         c = conn.cursor()
-        c.execute("SELECT user_id,title,image,main_evaluation,main_comment,location,category,date from posts WHERE user_id = ?",(user_id,))
+        c.execute("SELECT user_id,title,image,main_evaluation,main_comment,location,category,date from posts WHERE post_id = ?",(post_id,))
         details = c.fetchone()
-        c.execute("SELECT name FROM users WHERE id = ?")
+        c.execute("SELECT name FROM users WHERE id = ?",(details[0],))
         user_name = c.fetchone() 
         conn.close()
         return render_template("introduce.html" , tpl_details = details, tpl_user_name = user_name)
 
-        if request.method == "GET":
-        if "user_id" in session
-            comment = request.form.get("comment")
-            return render_template("introduce.html")
+        if request.method == "POST":
+            if "user_id" in session
+                user_id = session["user_id"]
+                comment = request.form.get("comment")
+                time = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+                conn = sqlite3.connect("graduation_work.db")
+                c = conn.cursor()
+                c.execute("INSERT INTO replys values (null,?,?,?,?)"(post_id,user_id,comment,time,))
+                conn.commit()
+                conn.close()
+                return render_template("introduce.html")
+
+        else:
+            conn = sqlite3.connect("graduation_work.db")
+            c = conn.cursor()
+            c.execute("SELECT users_id, others_comment, date FROM replys WHERE posts_id = ?",(post_id,))
+            reply_info = c.fetchone()
+            return render_template("introduce.html" reply_info = reply_info)
+
+
+        
 
 
 
