b5002099dc35f836a1167565b7731c0124e7ad49
美里海登 <kaitwun@misatokaitounoMacBook-Pro.local>
2021-03-05 12:08:56 +0900

    del mika.py

--- each_program/mika.py
+++ each_program/mika.py
@@ -7,21 +7,4 @@ from flask import Flask , render_template , request , redirect , session, url_fo
 #Blueprintオブジェクトを生成
 app = Blueprint('mika' , __name__)
 
-# @app.route("/level1", ["GET"])
-# def level1():
-#     conn = sqlite3.connect("graduation_work.db")
-<<<<<<< HEAD
-# <<<<<<< HEAD
-#     c = conn.cursor()
-# =======
-=======
->>>>>>> cd0f59b9de5e5753e2b9277a26615413670528a8
-#     c = conn.cursor()
-#     c.execute("select id, image, title from posts where main_evaluation = level 1")
-#     post = []
-#     for item in c.fetchall():
-#         post.append({"post_id":item[0],"image":item[1],"title":item[2]})
-<<<<<<< HEAD
-# >>>>>>> 95bca05df9eb8345f704e54576c47f80e50604e9
-=======
->>>>>>> cd0f59b9de5e5753e2b9277a26615413670528a8
+
